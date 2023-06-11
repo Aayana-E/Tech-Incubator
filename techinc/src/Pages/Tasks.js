16243c905f360bed085e7050c895e6b14f9e5ec9
Amritpal Singh <114454973+AmritS7@users.noreply.github.com>
2023-06-11 19:35:51 -0400

    Update Tasks.js

--- techinc/src/Pages/Tasks.js
+++ techinc/src/Pages/Tasks.js
@@ -1,1 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import firebaseConfig from '../backend/firebaseConfig';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardFooter
+  } from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
+
+function Tasks() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const tasksRef = firebase.database().ref('tasks');
+    tasksRef.on('value', (snapshot) => {
+      const tasksData = snapshot.val();
+      const tasksArray = Object.entries(tasksData || {}).map(([taskId, task]) => ({
+        id: taskId,
+        ...task,
+      }));
+      setTasks(tasksArray);
+    });
+
+    return () => {
+      tasksRef.off();
+    };
+  }, []);
+
+  return (
+    <div className="card-deck">
+      {tasks.map((task) => (
+         <center>
+         <MDBCard style={{ maxWidth: "15rem" }}>
+           <MDBCardBody>
+             <MDBCardTitle>{task.title}</MDBCardTitle>
+             <MDBCardText>
+                {task.description}
+             </MDBCardText>
+             <MDBCardFooter className='text-muted'>{task.company} </MDBCardFooter>
+           </MDBCardBody>
+         </MDBCard>
+       </center>
+      ))}
+    </div>
+  );
+}
+
+export default Tasks;
