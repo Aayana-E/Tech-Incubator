8948ed46da259b358a00b8bb401605c6f3d02a05
Aayana <annaevanson53@gmail.com>
2023-06-12 11:34:57 -0400

    Merging over Ahsan Files and Missing Modules
    Merging Ahsan Files from branch

--- techinc/src/Components/Navbar.js
+++ techinc/src/Components/Navbar.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Logo from "../Images/Background.jpg";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
-function Navbar() {
+export const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />
+    },
+    {
+      text: "About",
+      icon: <InfoIcon />
+    },
+    {
+      text: "Register",
+      icon: <CommentRoundedIcon />
+    }
+  ];
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
 
-export default Navbar
+      <div className="nav-links-container">
+        <List>
+          {menuOptions.map((option, index) => (
+            <ListItem key={index} button>
+              <ListItemIcon>{option.icon}</ListItemIcon>
+              <ListItemText primary={option.text} />
+            </ListItem>
+          ))}
+        </List>
+
+        <button className="primary-button">Register Now</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- techinc/src/Pages/Home.js
+++ techinc/src/Pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Navbar } from '../Components/Navbar'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div classname= "home-container">
+        <Navbar/>
+    </div>
   )
 }
 
